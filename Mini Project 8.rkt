87088bc9908e74fab243d6865431fcd229c51d8e
RingOfProgrammers <elliotbb@believershome.com>
2021-12-02 20:17:52 -0600

    Update Mini Project 8.rkt

--- Mini Project 8.rkt
+++ Mini Project 8.rkt
@@ -376,10 +376,7 @@
 ;;;   '#hash(
 ;;;     (singular . (...list_of_singular_nouns...)
 ;;;     (plural . (...list_of_plural_nouns...))))
-(define file->noun-dictionary
-  (lambda (filename)
-    (hash 'singular (csv->column-list filename 0)
-          'plural (csv->column-list filename 1))))
+
 
 
 
@@ -387,15 +384,13 @@
 ;;;   personal-pronouns : string? that is a valid file name
 ;;;   other-pronouns : string? that is a valid file name
 ;;; personal-pronouns should be formatted as follows
-;;;   i,me,my,mine,myself
-;;;   we,us,out,ours,ourself
-;;;   thou,thee,thy,thine,thineself
-;;;   you,you,your,yours,yourself
-;;;   ye,you,your,yours,yourself
-;;;   he,him,his,his,himself
-;;;   she,her,her,hers,herself
-;;;   it,it,its,--,itself
-;;;   they,them,their,theirs,themselves
+;;;   i,we,thou,you,ye,he,she,it,they
+;;;   me,us,thee,you,you,him,her,it,them
+;;;   my,our,thy,your,your,his,her,its,their
+;;;   mine,ours,thine,yours,yours,his,hers,,theirs
+;;;   myself,ourselves,thyself,yourself,yourselves,himself,herself,itself,themselves
+
+;;;   
 ;;; other-pronouns should be formatted as follows
 ;;;   indefinite1,indefinite2,...
 ;;;   demonstrative1,demonstrative2,...
@@ -416,20 +411,40 @@
 ;;;     (demonstrative . (...list_of_demonstrative_pronouns...))
 ;;;     (interrogative . (...list_of_interrogative_pronouns...))
 ;;;     (relative . (...list_of_relative_pronouns...)))
+(define file->pronouns-dictionary
+  (lambda (personal-pronouns other-pronouns)
+    (hash '1S (csv->column-list personal-pronouns 0)
+          '1P (csv->column-list personal-pronouns 1)
+          '2AS (csv->column-list personal-pronouns 2)
+          '2S (csv->column-list personal-pronouns 3)
+          '2P (csv->column-list personal-pronouns 4)
+          '3M (csv->column-list personal-pronouns 5)
+          '3F (csv->column-list personal-pronouns 6)
+          '3N (csv->column-list personal-pronouns 7)
+          '3P (csv->column-list personal-pronouns 8)
+          'indefinite (csv->column-list other-pronouns 0)
+          'demonstrative (csv->column-list other-pronouns 1)
+          'interrogative (csv->column-list other-pronouns 2)
+          'relative (csv->column-list other-pronouns 3))))
 
 
 
 
-;;; (file->other-dictionary adjectives adverbs prepositions cojuctions interjections)
-;;;    all parameters : string? that is a valid file name
-;;; All files should be formatted as a new-line separated list of words
-;;; TODO Add articles (the, an, a) to adjective list
-;;; Dictionary hash will be formatted as follows
+;;; (file->noun-dictionary filename) -> hash?
+;;;   filename : string? that is a valid file name
+;;; filename should be formatted as follows
+;;;   word1_singular,word1_plural
+;;;   word2_singular,word2_plural
+;;;   ...
+;;; (e.g. https://raw.githubusercontent.com/djstrong/nouns-with-plurals/master/noun.csv)
+;;; Dictionary hash  will be formatted as follows
 ;;;   '#hash(
-;;;     (adjective . (...list_of_adjectives...))
-;;;     (adverb . (...list_of_adverbs...))
-;;;     (preposition . (...list_of_prepositions...))
-;;;     (interjection . (...list_of_interjectios...)))
+;;;     (singular . (...list_of_singular_nouns...)
+;;;     (plural . (...list_of_plural_nouns...))))
+(define file->noun-dictionary
+  (lambda (filename)
+    (hash 'singular (csv->column-list filename 0)
+          'plural (csv->column-list filename 1))))
 
 
 
@@ -439,6 +454,7 @@
 ;;;   dictionary : hash? formatted as noun dictionary
 ;;; If the given word is in the noun dictionary, it returns a word
 ;;;  struct with noun properties.  Othwerise, it returns #f
+
 
 
 
