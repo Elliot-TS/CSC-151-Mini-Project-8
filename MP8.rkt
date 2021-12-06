084a0ed07de2b12b3ae06b30559ac9b789d673f4
RingOfProgrammers <elliotbb@believershome.com>
2021-12-05 18:36:17 -0600

    Update MP8.rkt

--- MP8.rkt
+++ MP8.rkt
@@ -5,10 +5,6 @@
 (require rackunit/text-ui)
 
 
-Main changes: conjugation struct updated
-              string->verb updated
-
-
 ;;; CSC-151 - Rebelsky
 ;;; Mini Project 8: Sentence and Word Analysis
 ;;; Date: November 19 - December 5
@@ -20,8 +16,8 @@ Main changes: conjugation struct updated
 
 #| Conjugation Struct |#
 
-;;; (conjugation verbal-parameter) -> conjugation
-;;;   verbal parameter : tense and person identifier ex: 'present-simple
+;;; (conjugation form) -> conjugation
+;;;   form : tense and person identifier ex: 'present-simple
 ;;; Stores information about a verb's conjugation
 (struct conjugation-kernel (form))
 (define conjugation
@@ -29,10 +25,10 @@ Main changes: conjugation struct updated
     (cond
       [(not (or (equal? 'base form)
                 (equal? 'present-simple form)
-                (equal? 'past-simple form)
+                (equal? 'past-simple form
                 (equal? 'past-participle form)
                 (equal? 'present-participle form)))
-       (error "conjugation :" form)]
+       (error "conjugation :" verbal-parameter)]
       [else
        (conjugation-kernel form)])))
 
@@ -124,7 +120,7 @@ Main changes: conjugation struct updated
       [else
        (word-kernel wordstr conjuclention)])))
 ; (word "cat" (conjuclention "noun" (declention "singular" "female" "objective")))
-;;; not currently working but it will. 
+;;; not currently working but it will.
 
 
 
@@ -180,15 +176,6 @@ Main changes: conjugation struct updated
 (define string->conjuclention
   (lambda (str)
     (declention 'singular 'neuter 'nominative)))
-
-;;; (string->word str) -> word?
-;;;    str : string?
-;;; Converts a string to a word struct.
-;;;  Assumes str contains one word
-(define string->word
-  (lambda (str)
-    (word str
-          (string->conjuclention str))))
 
 
 ;;; (string->words-list str) -> list? of string?
@@ -278,7 +265,7 @@ Main changes: conjugation struct updated
              '("I'm a little tea pot.\t short and stout"))
 (test-equal? "single new-line"
              (string->paragraph-list "I'm a little tea pot.\n short and stout")
-             '("I'm a little tea pot.\n short and stout"))                                    
+             '("I'm a little tea pot.\n short and stout"))
 
 ;;; (string->sentence str) -> sentence?
 ;;;   str : string?
@@ -335,8 +322,8 @@ Main changes: conjugation struct updated
 ;;; (file->verb-dictioary filename)
 ;;;   filename : string? that is a valid file name
 ;;; filename should be formatted as follows
-;;;   base1,present_simple1,past_simple1,past_participle1,present_participle1   
-;;;   base2,present_simple2,past_simple2,past_participle2,present_participle2   
+;;;   base1,present_simple1,past_simple1,past_participle1,present_participle1
+;;;   base2,present_simple2,past_simple2,past_participle2,present_participle2
 ;;;   ...
 ;;; Dictionay hash will be formatted as follows
 ;;;   '#hash(
@@ -349,7 +336,7 @@ Main changes: conjugation struct updated
   (lambda (filename)
     (hash 'base (csv->column-list filename 0)
           'present-simple (csv->column-list filename 1)
-          'past-simple (csv->column-list filename 2)          
+          'past-simple (csv->column-list filename 2)
           'past-participle (csv->column-list filename 3)
           'present-participle (csv->column-list filename 4))))
 
@@ -368,7 +355,7 @@ Main changes: conjugation struct updated
 ;;;   mine,ours,thine,yours,yours,his,hers,,theirs
 ;;;   myself,ourselves,thyself,yourself,yourselves,himself,herself,itself,themselves
 
-;;;   
+;;;
 ;;; other-pronouns should be formatted as follows
 ;;;   indefinite1,indefinite2,...
 ;;;   demonstrative1,demonstrative2,...
@@ -378,7 +365,7 @@ Main changes: conjugation struct updated
 ;;;   '#hash(
 ;;;     (1S . (nominative objective possessive-adjective possessive reflexive)) ; (1st singular)
 ;;;     (1P . (nominative objective possessive-adjective possessive reflexive)) ; (1st plural)
-;;;     (2AS . (nominative objective possessive-adjective possessive reflexive)) ; (2nd archaic singular) 
+;;;     (2AS . (nominative objective possessive-adjective possessive reflexive)) ; (2nd archaic singular)
 ;;;     (2S . (nominative objective possessive-adjective possessive reflexive)) ; (2nd singular)
 ;;;     (2P . (nominative objective possessive-adjective possessive reflexive)) ; (2nd plural)
 ;;;     (3M . (nominative objective possessive-adjective possessive reflexive)) ; (3rd masculine singular)
@@ -472,24 +459,24 @@ Main changes: conjugation struct updated
 
 
 
+
 ;;; (string->verb str dictionary)
-;;;
-;;;
-;;; (conjugation verbal-parameter) -> conjugation
-;;;   verbal parameter : tense and person identifier ex: 'present-simple
-;;; Stores information about a verb's conjugation
+;;;   str : string? that is one word
+;;;   dictionary : hash? formatted as verb dictionary
+;;; If the given word is in the verb dictionary, it returns a word
+;;;  struct with noun properties.  Othwerise, it returns #f
 (define string->verb
   (lambda (str dictionary)
-    (cond [(number? (index-of str (hash-ref dictionary 'base)))
-           (word str (conjuclension "verb" (conjugation 'infinitive)))]
-          [(number? (index-of str (hash-ref dictionary 'present-simple)))
-           (word str (conjuclension "verb" (conjugation 'present-simple)))]
-          [(number? (index-of str (hash-ref dictionary 'past-simple)))
-           (word str (conjuclension "verb" (conjugation 'past-simple)))]
-          [(number? (index-of str (hash-ref dictionary 'past-participle)))
-           (word str (conjuclension "verb" (conjugation 'past-participle)))]
-          [(number? (index-of str (hash-ref dictionary 'present-participle)))
-           (word str (conjuclension "verb" (conjugation 'present-participle)))]
+    (cond [(equal? str (hash-ref dictionary 'base))
+           (list str 'base)]
+          [(equal? str (hash-ref dictionary 'present-simple))
+           (list str 'present-simple (list-ref (hash-ref dictionary 'base) (index-of str (hash-ref dictionary 'present-simple))))]
+          [(equal? str (hash-ref dictionary 'past-simple))
+           (list str 'past-simple (list-ref (hash-ref dictionary 'base) (index-of str (hash-ref dictionary 'past-simple))))]
+          [(equal? str (hash-ref dictionary 'past-participle))
+           (list str 'past-participle (list-ref (hash-ref dictionary 'base) (index-of str (hash-ref dictionary 'past-participle))))]
+          [(equal? str (hash-ref dictionary 'present-participle))
+           (list str 'present-participle (list-ref (hash-ref dictionary 'base) (index-of str (hash-ref dictionary 'present-participle))))]
           [else
            #f])))
 
@@ -503,53 +490,66 @@ Main changes: conjugation struct updated
   (lambda (str dictionary)
     (cond [(equal? str (hash-ref dictionary '1S))
            (list str '1S)]
-          [(number? (index-of str (hash-ref dictionary '1P)))
+          [(equal? str (hash-ref dictionary '1P))
            (list str '1P)]
-          [(number? (index-of str (hash-ref dictionary '2AS)))
+          [(equal? str (hash-ref dictionary '2AS))
            (list str '2AS)]
-          [(number? (index-of str (hash-ref dictionary '2S)))
+          [(equal? str (hash-ref dictionary '2S))
            (list str '2S)]
-          [(number? (index-of str (hash-ref dictionary '2P)))
+          [(equal? str (hash-ref dictionary '2P))
            (list str '2P)]
-          [(number? (index-of str (hash-ref dictionary '3M)))
+          [(equal? str (hash-ref dictionary '3M))
            (list str '3M)]
-          [(number? (index-of str (hash-ref dictionary '3F)))
+          [(equal? str (hash-ref dictionary '3F))
            (list str '3F)]
-          [(number? (index-of str (hash-ref dictionary '3N)))
+          [(equal? str (hash-ref dictionary '3N))
            (list str '3N)]
-          [(number? (index-of str (hash-ref dictionary '3P)))
+          [(equal? str (hash-ref dictionary '3P))
            (list str '3P)]
-          [(number? (index-of str (hash-ref dictionary 'indefinite)))
+          [(equal? str (hash-ref dictionary 'indefinite))
            (list str 'indefinite)]
-          [(number? (index-of str (hash-ref dictionary 'demonstrative)))
+          [(equal? str (hash-ref dictionary 'demonstrative))
            (list str 'demonstrative)]
-          [(number? (index-of str (hash-ref dictionary 'interrogative)))
+          [(equal? str (hash-ref dictionary 'interrogative))
            (list str 'interrogative)]
-          [(number? (index-of str (hash-ref dictionary 'relative)))
+          [(equal? str (hash-ref dictionary 'relative))
            (list str 'relative)]
           [else
            #f])))
 
 
-;;; TODO Rewrite string->word to work using the above functions as well as the other words dictionary
-;;;I can't figure out how to write this one :/
+
+
+;;; (string->word str) -> word?
+;;;    str : string?
+;;; Converts a string to a word struct.
+;;;  Assumes str contains one word
 (define string->word
   (lambda (word)
     (let ([noun? (string->noun word)]
           [verb? (string->verb word)]
-          [pronoun? (string->pronoun word)]
-          [adjective? (string->adjective word)])   
-      (cond 
+          [pronoun? (string->pronoun word)])
+      (cond
         [noun?
          noun?]
         [verb?
          verb?]
-        [adjective
-         adjective?]
         [pronoun?
          pronoun?]
+        [(index-of (hash-ref other-dictionary
+                             'adjective))
+         (word "adjective" null)]
+        [(index-of (hash-ref other-dictionary
+                             'adverb))
+         (word "adverb" null)]
+        [(index-of (hash-ref other-dictionary
+                             'preposition))
+         (word "preposition" null)]
+        [(index-of (hash-ref other-dictionary
+                             'conjunction))
+         (word "conjunction" null)]
         [else
-         unknown])))
+         (word "unknown" null)]))))
 
 (define string->words
   (map string->word (file->paragraphs)))
